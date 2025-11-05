8125761fdfea36707f42930cc83735c6ecf54e3a
Camille  Bujotzek <camillebujotzek@MacBook-Air-de-Camille.local>
2025-11-05 17:59:15 +0100

    mise à jour

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import { Recette } from "./datas/recette";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,10 +15,17 @@ app.use(express.json());
 
 app.use("/assets", express.static(path.join(__dirname, "/assets")));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/entrée", (req, res) => {
+  res.json(Recette);
 });
 
+app.get("/plat", (req, res) => {
+  res.json(Recette);
+}); 
+
+app.get("/dessert", (req, res) => {
+  res.json(Recette);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
